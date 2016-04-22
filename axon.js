edae7c4c378a7a810490cebfec9195e4c56b0f1d
Danny Thuering <dthuering@gmail.com>
2016-04-22 19:42:03 +0200

    try to read beacon data

--- axon.js
+++ axon.js
@@ -1,18 +1,22 @@
-(function(){
+(function() {
 
-	//call the UI page "home"
-	App.load('home');
-	
-	setInterval(function(){
-	
-		$.getJSON( "http://localhost:8080/nervousnet-api/raw-sensor-data/GPS", function( data ) {
-			
-			$("#sensordata").html(JSON.stringify(data));
-			
-		});
-		
-		
-	}, 1000);
-	
+    //call the UI page "home"
+    App.load('home');
+
+    setInterval(function() {
+        $.getJSON("http://localhost:8080/nervousnet-api/raw-sensor-data/GPS", function(data) {
+            $("#sensordata").html(JSON.stringify(data));
+        });
+    }, 1000);
+
+		function getBeaconData() {
+			$.getJSON("http://localhost:8080/nervousnet-api/raw-sensor-data/Beacon", function(data) {
+					$("#beacon").html(JSON.stringify(data));
+			});
+		}
+
+		setInterval(function() {
+			getBeaconData();
+		}, 1000);
 
 })();
