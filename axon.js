b49335f37799c665cce2ca346c3035930e425aa4
Danny Thuering <dthuering@gmail.com>
2016-04-23 11:19:38 +0200

    u

--- axon.js
+++ axon.js
@@ -43,7 +43,7 @@
 			if(dist < 1) {
 				folder = "inside";
 			}
-			var url = "https://192.168.43.78.xip.io:3000/"+ folder +"/"+ cb.uuid +"/"+ deviceid;
+			var url = "http://192.168.43.78.xip.io:3000/"+ folder +"/"+ cb.uuid +"/"+ deviceid;
 			$("#uuid").html(url);
 				// $.getJSON(url, function(response) {
 				// 	$.post("http://localhost:8080/nervousnet-api/log", JSON.stringify(response));
