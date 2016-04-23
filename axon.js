73ab368d218eddef3de7369469ddabcff782be85
Danny Thuering <dthuering@gmail.com>
2016-04-23 11:12:06 +0200

    use ssl

--- axon.js
+++ axon.js
@@ -43,14 +43,14 @@
 			if(dist < 1) {
 				folder = "inside";
 			}
-			var url = "http://192.168.43.78.xip.io:3000/"+ folder +"/"+ cb.uuid +"/"+ deviceid;
+			var url = "https://192.168.43.78.xip.io:3000/"+ folder +"/"+ cb.uuid +"/"+ deviceid;
 			$("#uuid").html(url);
 				// $.getJSON(url, function(response) {
 				// 	$.post("http://localhost:8080/nervousnet-api/log", JSON.stringify(response));
 				// 	$("#response").html(JSON.stringify(response));
 				// });
 
-				if(cb.uuid) {
+				if(cb.uuid && dist < 10) {
 					$.ajax({
 						url: url
 					})
